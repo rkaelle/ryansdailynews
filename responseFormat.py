e28f16ccfb80d775a7a6c6d71e39712047a13e3b
rkalesalad <rkaelle2@gmail.com>
2024-07-26 11:00:54 -0700

    mobile scaling & fine tuned responses

--- responseFormat.py
+++ responseFormat.py
@@ -8,7 +8,7 @@ def process_message_with_openai(message):
 
     # Define the conversation context and user message
     messages = [
-        {"role": "system", "content": "Talk in first person as the person who sent the message. You should organize and extract the information. Make the informal text into a nice looking chunk of words. "},
+        {"role": "system", "content": "Talk in first person as the person who sent the message. You should organize and extract the information. Make the informal text into a nice looking chunk of words. Do not mention anything like 'From the message,' or ' it appears that the sender said'"},
         {"role": "user", "content": f"Please analyze the following message and provide structured responses for each of these queries:\n\nMessage: '{message.body}'\n\n1. Identify any skill being learned from the message and make sure to include the word learning.\n2. Provide a reflection on yesterday based on the message and make sure to include \"reflecting on yesterday\" \n3. Note any favorite food mentioned in the message and make sure to include \"My favorite food from yesterday was\" Additioanlly, say a random science fun fact and make it the 4th bullet point (but dont include any intro such as: \"Here's a fun science fact:\"). 4."}
     ]
 
